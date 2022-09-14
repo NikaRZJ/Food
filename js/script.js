5bac250f2dc38d754957987258c1d51ab4349b87
NikaRZJ <nikar_50@mail.ru>
2022-09-14 15:38:07 +0700

    add slider

--- js/script.js
+++ js/script.js
@@ -184,9 +184,22 @@ document.addEventListener('DOMContentLoaded', () => {
         return await res.json();
     };
 
-    getResource('http://localhost:3000/menu')
+    // getResource('http://localhost:3000/menu')
+    //     .then(data => {
+    // data.forEach(({
+    //     img,
+    //     altimg,
+    //     title,
+    //     descr,
+    //     price
+    // }) => {
+    //     new MenuCard(img, altimg, title, descr, price, '.menu .container').render();
+    // });
+    //     });
+
+    axios.get('http://localhost:3000/menu')
         .then(data => {
-            data.forEach(({
+            data.data.forEach(({
                 img,
                 altimg,
                 title,
@@ -309,4 +322,113 @@ document.addEventListener('DOMContentLoaded', () => {
     fetch('http://localhost:3000/menu')
         .then(data => data.json())
         .then(res => console.log(res));
+
+    // Slider
+    const slides = document.querySelectorAll('.offer__slide'),
+        prevBtn = document.querySelector('.offer__slider-prev'),
+        nextBtn = document.querySelector('.offer__slider-next'),
+        slidesWrapper = document.querySelector('.offer__slider-wrapper'),
+        slidesField = document.querySelector('.offer__slider-inner'),
+        width = window.getComputedStyle(slidesWrapper).width,
+        current = document.getElementById('current'),
+        total = document.getElementById('total');
+
+    let slideIndex = 1,
+        offset = 0;
+
+
+    slidesField.style.width = 100 * slides.length + '%';
+    slidesField.style.display = 'flex';
+    slidesField.style.transition = '0.5s all';
+
+    slidesWrapper.style.overflow = 'hidden';
+
+    slides.forEach(slide => {
+        slide.style.width = width;
+    });
+
+    if (slides.length < 10) {
+        total.textContent = `0${slides.length}`;
+        current.textContent = `0${slideIndex}`;
+    } else {
+        total.textContent = slides.length;
+        current.textContent = slideIndex;
+    }
+
+    nextBtn.addEventListener('click', () => {
+        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)) {
+            offset = 0;
+        } else {
+            offset += +width.slice(0, width.length - 2);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == slides.length) {
+            slideIndex = 1;
+        } else {
+            slideIndex++;
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    });
+
+    prevBtn.addEventListener('click', () => {
+        if (offset == 0) {
+            offset = +width.slice(0, width.length - 2) * (slides.length - 1);
+        } else {
+            offset -= +width.slice(0, width.length - 2);
+        }
+
+        slidesField.style.transform = `translateX(-${offset}px)`;
+
+        if (slideIndex == 1) {
+            slideIndex = slides.length;
+        } else {
+            slideIndex--;
+        }
+
+        if (slides.length < 10) {
+            current.textContent = `0${slideIndex}`;
+        } else {
+            current.textContent = slideIndex;
+        }
+    });
+
+    // function showSlide(index) {
+    //     index = Number(index);
+    //     slides[index - 1].classList.add('show');
+    //     slides[index - 1].classList.remove('hide');
+    //     slides.forEach((item, i) => {
+    //         if (i != index - 1) {
+    //             item.classList.add('hide');
+    //         }
+    //     });
+    // }
+
+    // showSlide(current.textContent);
+
+    // prevBtn.addEventListener('click', () => {
+    //     let index = Number(current.textContent);
+    //     if (index > 1) {
+    //         current.textContent = `0${index - 1}`;
+    //         index--;
+    //         showSlide(index);
+    //     }
+    // });
+
+    // nextBtn.addEventListener('click', () => {
+    //     let index = Number(current.textContent);
+    //     if (index < 4) {
+    //         current.textContent = `0${index + 1}`;
+    //         index++;
+    //         showSlide(index);
+    //     }
+    // });
+
+
 });
